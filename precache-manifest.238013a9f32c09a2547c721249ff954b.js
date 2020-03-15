90735399a36c9a9b8f4cd12b11e892e1be961fef
Arif Hussain <48639605+arifhussain123@users.noreply.github.com>
2020-03-15 12:35:48 +0500

    Add files via upload

---
+++ precache-manifest.238013a9f32c09a2547c721249ff954b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "371d6f26065d263ac49cbd94cfcc110b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbdd77689ed80c4b436a",
+    "url": "/static/css/main.d899e17a.chunk.css"
+  },
+  {
+    "revision": "e3d77fe251e5ca1400ba",
+    "url": "/static/js/2.f0690f22.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.f0690f22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbdd77689ed80c4b436a",
+    "url": "/static/js/main.d1efcf71.chunk.js"
+  },
+  {
+    "revision": "76045e400e3c170686df",
+    "url": "/static/js/runtime-main.ef2c9948.js"
+  }
+]);
